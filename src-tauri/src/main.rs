318e779be244ebeabe2134d060e73ca149f3b6eb
fengzhongshaonian <2844126834@qq.com>
2024-05-21 08:30:28 +0800

    hide app icon from taskbar and add tray icon and hide window frame

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,8 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::{CustomMenuItem, Manager, SystemTray, SystemTrayMenu};
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -9,6 +11,15 @@ fn greet(name: &str) -> String {
 
 fn main() {
     tauri::Builder::default()
+        .setup(|app|{
+            let main_window = app.get_window("main").unwrap();
+            main_window.set_skip_taskbar(true).unwrap(); // This doesn't work when using wayland!
+            Ok(())
+        })
+        .system_tray(SystemTray::new().with_menu(
+            SystemTrayMenu::new()
+                .add_item(CustomMenuItem::new("quit", "Quit"))
+        ))
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
