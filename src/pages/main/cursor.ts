173b47a43bd27d5c0e4f3298de2b62099a0d459d
fengzhongshaonian <2844126834@qq.com>
2024-05-29 10:57:30 +0800

    自定义鼠标光标

---
+++ src/pages/main/cursor.ts
@@ -0,0 +1,293 @@
+
+type MouseEventListener = (event: MouseEvent) => void;
+
+type CursorLeftTopPos = { left: number, top: number };
+
+export abstract class Cursor {
+    protected mouse: HTMLElement;
+
+    // 光标应用于该元素，该元素提供鼠标移动相关事件的HTML元素，光标通过监听它从而移动自己
+    private cursorEffectElement: HTMLElement;
+
+    private mouseMoveEventListener: MouseEventListener | null = null;
+    private mouseOutEventListener: MouseEventListener | null = null;
+
+    protected constructor(cursorEffectElement: HTMLElement) {
+        // TODO：动态创建mouse元素
+        let mouse = document.getElementById('mouse');
+        if (mouse == null) {
+            throw new Error('HTMLElement with id "mouse" not found');
+        }
+        this.mouse = mouse;
+        this.cursorEffectElement = cursorEffectElement;
+    }
+
+    active() {
+        this.updateCursorShape();
+        this.mouseMoveEventListener = (event) => {
+            this.onMouseMove(event);
+        };
+        this.mouseOutEventListener = () => {
+            this.onMouseOut();
+        }
+        this.cursorEffectElement.style.cursor = 'none';
+        this.cursorEffectElement.addEventListener('mousemove', this.mouseMoveEventListener);
+        this.cursorEffectElement.addEventListener('mouseout', this.mouseOutEventListener);
+    }
+
+    deactive() {
+        if (this.mouseMoveEventListener != null) {
+            this.cursorEffectElement.removeEventListener('mousemove', this.mouseMoveEventListener);
+        }
+        if (this.mouseOutEventListener != null) {
+            this.cursorEffectElement.removeEventListener('mouseout', this.mouseOutEventListener);
+        }
+        this.cursorEffectElement.style.cursor = 'auto';
+    }
+
+    onMouseMove(event: MouseEvent) {
+        const {left, top} = this.calculateCursorLeftTopPos(event);
+        this.mouse.style.left = left + 'px';
+        this.mouse.style.top = top + 'px';
+    }
+
+    onMouseOut() {
+        this.mouse.style.left = '-50%';
+        this.mouse.style.top = '-50%';
+    }
+
+    // 更新光标的形状
+    abstract updateCursorShape(): void;
+
+    // 计算光标的位置
+    abstract calculateCursorLeftTopPos(event: MouseEvent): CursorLeftTopPos;
+}
+
+export interface BoundingRect {
+    width: number,
+    height: number
+}
+
+// 十字形光标的样式
+export interface CrossHairStyle {
+    // 线条的宽度
+    strokeWidth: number,
+    // 线条的颜色
+    strokeColor: string,
+    // 光标外接矩形（可以想象十字形光标是“田”中的“十”，而boundingRectWidth表示其中的“口”的宽度）
+    boundingRectWidth: number,
+    // 光标外接矩形（可以想象十字形光标是“田”中的“十”，而boundingRectHeight表示其中的“口”的高度）
+    boundingRectHeight: number,
+}
+
+// 十字形光标
+export class CrossHair extends Cursor {
+
+    private readonly _style: CrossHairStyle;
+
+    constructor(cursorEffectElement: HTMLElement, style?: CrossHairStyle) {
+        super(cursorEffectElement);
+
+        const cursor = this;
+        const handler: ProxyHandler<CrossHairStyle> = {
+            get(target: CrossHairStyle, propKey: string) {
+                return Reflect.get(target, propKey);
+            },
+            set(target: CrossHairStyle, propKey: string , newValue: any) {
+                Reflect.set(target, propKey, newValue);
+                cursor.updateCursorShape();
+                return true;
+            }
+        };
+        if(style){
+            this._style = new Proxy(style, handler);
+        }else {
+            this._style = new Proxy({
+                strokeWidth: 1,
+                strokeColor: 'red',
+                boundingRectWidth: 24,
+                boundingRectHeight: 24
+            }, handler);
+        }
+    }
+
+    getShape() {
+        return `
+          <svg xmlns="http://www.w3.org/2000/svg">
+            <line
+              x1="0"    
+              y1="${this._style.boundingRectHeight / 2}" 
+              x2="${this._style.boundingRectWidth}"     
+              y2="${this._style.boundingRectHeight / 2}"    
+              stroke="${this._style.strokeColor}"        
+              stroke-width="${this._style.strokeWidth}"  
+            />
+            <line
+              x1="${this._style.boundingRectWidth / 2}"    
+              y1="0"     
+              x2="${this._style.boundingRectWidth / 2}"     
+              y2="${this._style.boundingRectHeight}"    
+              stroke="${this._style.strokeColor}"        
+              stroke-width="${this._style.strokeWidth}"
+            />
+          </svg>
+    `;
+    }
+
+    calculateCursorLeftTopPos(event: MouseEvent): CursorLeftTopPos {
+        const left = (event.pageX - this._style.boundingRectWidth / 2);
+        const top = (event.pageY - this._style.boundingRectHeight / 2);
+        return {
+            left, top
+        }
+    }
+
+    updateCursorShape(): void {
+        const shape = this.getShape();
+        console.log('CrossHair shape:', shape);
+
+        const encoded = btoa(shape);
+        const dataURL = `data:image/svg+xml;base64,${encoded}`
+
+        this.mouse.style.background = `url(${dataURL}) no-repeat center center`;
+        this.mouse.style.width = this._style.boundingRectWidth + 'px';
+        this.mouse.style.height = this._style.boundingRectHeight + 'px';
+    }
+
+    get style(){
+        return this._style;
+    }
+}
+
+// 带圆圈的数字光标的样式
+interface CircleNumberStyle {
+    // 圆圈中的数字
+    num: number,
+    // 圆圈的线条宽度
+    strokeWidth: number,
+    // 圆圈的线条颜色
+    strokeColor: string,
+    // 圆圈的填充颜色
+    fillColor: string,
+    // 圆圈中数字的字体，如：'Arial'
+    fontName: string,
+    // 圆圈中数字的字体大小，如：'16px'
+    fontSize: string,
+    // 圆圈中数字的字体颜色
+    fontColor: string,
+    // 圆圈中的数字到圆圈之间的空隙的最小值
+    padding: number
+}
+
+// 带圆圈的数字光标
+export class CircleNumber extends Cursor {
+
+    private _style: CircleNumberStyle;
+    private _diameter: number;
+
+    constructor(cursorEffectElement: HTMLElement, style?: CircleNumberStyle) {
+        super(cursorEffectElement);
+        const cursor = this;
+        const handler: ProxyHandler<CircleNumberStyle> = {
+            get(target: CircleNumberStyle, propKey: string) {
+                return Reflect.get(target, propKey);
+            },
+            set(target: CircleNumberStyle, propKey: string , newValue: any) {
+                Reflect.set(target, propKey, newValue);
+                cursor.updateCursorShape();
+                return true;
+            }
+        };
+        if(style){
+            this._style = new Proxy(style, handler);
+        }else {
+            this._style = new Proxy({
+                num: 123,
+                strokeWidth: 0,
+                strokeColor: 'red',
+                fillColor: 'red',
+                fontName: 'Arial',
+                fontSize: '16px',
+                fontColor: 'white',
+                padding: 8
+            }, handler);
+        }
+        const textMetrics = this.detectTextWidth();
+        this._diameter = this.calculateDiameter(textMetrics);
+    }
+
+    getShape(textMetrics: TextMetrics) {
+        const diameter = this.calculateDiameter(textMetrics);
+
+        // 半径
+        const radius = this._diameter / 2;
+        return `
+          <svg xmlns="http://www.w3.org/2000/svg">
+            <circle
+              cx="${radius}"
+              cy="${radius}"
+              r="${radius}"
+              fill="${this._style.fillColor}"
+              stroke="${this._style.strokeColor}"
+              stroke-width="${this._style.strokeWidth}" 
+            />
+            <text
+              x="${radius}" 
+              y="${radius+(textMetrics.actualBoundingBoxAscent)/2}"  
+              font-family="${this._style.fontName}"  
+              font-size="${this._style.fontSize}"        
+              fill="${this._style.fontColor}"        
+              text-anchor="middle"     
+            >
+              ${this._style.num}
+            </text>
+          </svg>
+       `;
+    }
+
+    // 计算圆圈的直径
+    private calculateDiameter(textMetrics: TextMetrics) {
+        const textWidth = textMetrics.actualBoundingBoxRight + textMetrics.actualBoundingBoxLeft;
+        const textHeight = textMetrics.actualBoundingBoxAscent + textMetrics.actualBoundingBoxDescent;
+        let max = Math.max(textWidth, textHeight)
+
+        return max + this._style.padding * 2;
+    }
+
+    detectTextWidth(): TextMetrics {
+        const canvas = document.createElement('canvas');
+        const context= canvas.getContext('2d');
+        if (context == null) {
+            throw new Error('Failed to getContext 2d');
+        }
+        context.font = `${this._style.fontSize} ${this._style.fontName}`;
+        context.textAlign= 'center';
+        return context.measureText(`${this._style.num}`);
+    }
+
+    calculateCursorLeftTopPos(event: MouseEvent): CursorLeftTopPos {
+        const diameter = this._diameter;
+        const left = (event.pageX - diameter/2);
+        const top = (event.pageY - diameter/2);
+        return {
+            left, top
+        }
+    }
+
+    updateCursorShape(): void {
+        const textMetrics = this.detectTextWidth();
+        console.log('textMetrics', textMetrics);
+
+        const shape = this.getShape(textMetrics);
+        console.log('CircleNumber shape:', shape);
+
+        const encoded = btoa(shape);
+        const dataURL = `data:image/svg+xml;base64,${encoded}`
+
+        this._diameter = this.calculateDiameter(textMetrics);
+        this.mouse.style.background = `url(${dataURL}) no-repeat center center`;
+        this.mouse.style.width = this._diameter + 'px';
+        this.mouse.style.height = this._diameter + 'px';
+    }
+
+}
--- src/pages/main/editor.ts
+++ src/pages/main/editor.ts
@@ -1,36 +1,49 @@
-import {App, Rect, Leafer, PointerEvent} from "leafer-ui";
-import logger from "../../common/logger.ts";
+import {App, Rect} from "leafer-ui";
+import {UI} from "@leafer-ui/display";
+import {CircleNumber, CrossHair, Cursor} from "./cursor.ts";
 
-function disableDragRegion(){
-    document.querySelector('#drag-region')?.removeAttribute('data-tauri-drag-region');
+function disableDragRegion() {
+    const element = document.getElementById('drag-region');
+    (element as HTMLDivElement).style.display = "none";
 }
 
-function enableDragRegion(){
-    document.querySelector('#drag-region')?.setAttribute('data-tauri-drag-region', '');
+function enableDragRegion() {
+    const element = document.getElementById('drag-region');
+    (element as HTMLDivElement).style.display = "absolute";
 }
 
-export interface ImageInfo{
+function getTouchpad() {
+    let element = document.getElementById('touchpad');
+    if (element == null) {
+        throw new Error('HTMLElement whose id="touchpad" not found');
+    }
+    return element;
+}
+
+export interface ImageInfo {
     dataURL: string,
     width: number,
     height: number
 }
 
 export class Editor {
-    private _app: App;
-    private _editing: boolean;
+    private readonly app: App;
+    private editing: boolean;
+    private annotationTool: AnnotationTool;
 
     constructor() {
-        this._app = new App({
+        this.app = new App({
             view: window,
-            ground: { type: 'draw' },
+            ground: {type: 'draw'},
             tree: {},
-            sky:  { type: 'draw' }
+            sky: {type: 'draw'}
         });
 
-        this._editing=false;
+        this.editing = false;
+        this.annotationTool = new RectangleTool(this.app, getTouchpad());
     }
 
-    open(image: ImageInfo){
+    open(image: ImageInfo) {
         const backgroundRect = new Rect({
             width: image.width,
             height: image.height,
@@ -39,64 +52,242 @@ export class Editor {
                 url: image.dataURL,
             }
         });
-        // this._app.ground.add(backgroundRect);
-        this._app.sky.add(backgroundRect);
+        this.app.ground.add(backgroundRect);
 
+        this.editing = true;
         disableDragRegion();
-
-        backgroundRect.on(PointerEvent.LONG_PRESS, (event: PointerEvent)=>{
-            logger.info('backgroundRect PointerEvent.LONG_PRESS, event:' + event).then()
-        });
+        this.annotationTool.active();
     }
 
-    get editing(): boolean {
-        return this._editing;
+    isEditing(): boolean {
+        return this.editing;
     }
 }
 
-export interface Context {
-
-    /**
-     * 鼠标光标
-     */
-    readonly cursor: string;
-
-    /**
-     * 颜色
-     */
-    readonly color: string;
-
-    readonly fontName: string;
-
-    readonly fontSize: number;
-
+interface StyleContext {
+    strokeWidth: number,
+    strokeColor: string,
+    // fontName: string,
+    // fontSize: number,
 }
 
-export abstract class AnnotationTool{
-    protected drawingBoard: Leafer | null = null;
+type MouseEventListener = (event: MouseEvent) => void;
+type WheelEventListener = (event: WheelEvent) => void;
+export abstract class AnnotationTool {
+    private readonly app: App;
+    // 触摸板，一个大小与窗口等同的HTML元素，用于感知鼠标事件
+    protected readonly touchpad: HTMLElement;
+
+    private mouseDownEventListener: MouseEventListener | null = null;
+    private mouseMoveEventListener: MouseEventListener | null = null;
+    private mouseUpEventListener: MouseEventListener | null = null;
+    private mouseOutEventListener: MouseEventListener | null = null;
+    private wheelEventListener: WheelEventListener | null = null;
+
+    protected styleContext: StyleContext;
+
+    protected constructor(app: App, touchpad: HTMLElement) {
+        this.app = app;
+        this.touchpad = touchpad;
+
+        this.styleContext = {
+            strokeWidth: 1,
+            strokeColor: 'red',
+            // fontName: '',
+            // fontSize: 1,
+        }
+    }
 
     /**
      * 每一个标注工具都要有独一无二的名称
      */
     abstract name(): string;
 
-    active(drawingBoard: Leafer): void{
-        this.drawingBoard = drawingBoard;
+    abstract onMouseDown(mouseDownEvent: MouseEvent): void;
+
+    abstract onMouseMove(mouseMoveEvent: MouseEvent): void;
+
+    abstract onMouseUp(mouseUpEvent: MouseEvent): void;
+
+    abstract onWheel(wheelEvent: WheelEvent): void;
+
+    /**
+     * 激活标注工具
+     */
+    active(): void {
+        this.mouseDownEventListener = (event) => {
+            this.onMouseDown(event);
+        }
+        this.mouseMoveEventListener = (event) => {
+            this.onMouseMove(event);
+        }
+        this.mouseUpEventListener = (event) => {
+            this.onMouseUp(event);
+        }
+        this.wheelEventListener = (event) => {
+            this.onWheel(event);
+        }
+
+        this.touchpad.addEventListener('mousedown', this.mouseDownEventListener);
+        this.touchpad.addEventListener('mousemove', this.mouseMoveEventListener);
+        this.touchpad.addEventListener('mouseup', this.mouseUpEventListener);
+        this.touchpad.addEventListener('wheel', this.wheelEventListener);
     }
 
     /**
-     * 按下鼠标左键
-     * @param x 鼠标的x坐标
-     * @param y 鼠标的y坐标
+     * 停用标注工具
      */
-    abstract pressTheLeftMouseButton(x: number, y: number): void;
+    deactive() {
+        if (this.mouseDownEventListener != null) {
+            this.touchpad.removeEventListener('mousedown', this.mouseDownEventListener);
+        }
+        if (this.mouseMoveEventListener != null) {
+            this.touchpad.removeEventListener('mousemove', this.mouseMoveEventListener);
+        }
+        if (this.mouseUpEventListener != null) {
+            this.touchpad.removeEventListener('mouseup', this.mouseUpEventListener);
+        }
+        if(this.mouseOutEventListener != null){
+            this.touchpad.removeEventListener('mouseout', this.mouseOutEventListener);
+        }
+        if(this.wheelEventListener != null){
+            this.touchpad.removeEventListener('wheel', this.wheelEventListener);
+        }
+    }
+
+    addToSky(child: UI) {
+        this.app.sky.add(child);
+    }
+
+    clearSky() {
+        this.app.sky.clear();
+    }
+
+    addToTree(child: UI) {
+        this.app.tree.add(child);
+    }
 }
 
-// class RectangleTool implements AnnotationTool {
-//
-//     name(): string {
-//         return "RectangleTool";
-//     }
-//
-//
-// }
+/**
+ * 判断鼠标事件中鼠标的左键是否处于被按下的状态
+ *
+ * @param mouseEvent 鼠标事件
+ */
+function mouseLeftButtonIsPressed(mouseEvent: MouseEvent) {
+    const LEFT_BUTTON = 1;
+    return mouseEvent.buttons == LEFT_BUTTON;
+}
+
+class RectangleTool extends AnnotationTool {
+
+    private rect: Rect | null = null;
+
+    private mouseDownEvent: MouseEvent | null = null;
+
+    // private customCursor: CrossHair;
+    private customCursor: CircleNumber;
+
+    constructor(app: App, touchpad: HTMLElement) {
+        super(app, touchpad);
+        // this.customCursor = new CrossHair(touchpad);
+        this.customCursor = new CircleNumber(touchpad);
+    }
+
+    name(): string {
+        return "RectangleTool";
+    }
+
+
+    active() {
+        super.active();
+        this.customCursor.active()
+    }
+
+
+    deactive() {
+        super.deactive();
+        this.customCursor.deactive();
+    }
+
+    onMouseDown(mouseDownEvent: MouseEvent): void {
+        if (!mouseLeftButtonIsPressed(mouseDownEvent)) {
+            return;
+        }
+        this.mouseDownEvent = mouseDownEvent;
+        this.rect = new Rect({
+            x: mouseDownEvent.x,
+            y: mouseDownEvent.y,
+            width: 0,
+            height: 0,
+            fill: "transparent",
+            stroke: this.styleContext.strokeColor,
+            strokeWidth: this.styleContext.strokeWidth
+        });
+        this.addToSky(this.rect);
+    }
+
+    onMouseMove(mouseMoveEvent: MouseEvent): void {
+        if (!mouseLeftButtonIsPressed(mouseMoveEvent)) {
+            // 鼠标左键未按下，如果此时 this.rect 不为null，
+            // 意味着此前鼠标可以在绘画过程中移出了touchpad，然后在touchpad之外松开了鼠标左键，
+            // 导致touchpad无法监听到mouseup事件，这里需要把之前绘制的图形提交到tree
+            if (this.rect) {
+                this.submit(this.rect);
+            }
+            return;
+        }
+        if (this.mouseDownEvent == null) {
+            return;
+        }
+        if (this.rect) {
+            let x = (mouseMoveEvent.x < this.mouseDownEvent.x) ? mouseMoveEvent.x : this.mouseDownEvent.x;
+            let y = (mouseMoveEvent.y < this.mouseDownEvent.y) ? mouseMoveEvent.y : this.mouseDownEvent.y;
+            let width = Math.abs(mouseMoveEvent.x - this.mouseDownEvent.x);
+            let height = Math.abs(mouseMoveEvent.y - this.mouseDownEvent.y);
+
+            this.rect.set({
+                x,
+                y,
+                width,
+                height,
+            });
+        }
+
+    }
+
+    onMouseUp(mouseUpEvent: MouseEvent): void {
+        if (!mouseLeftButtonIsPressed(mouseUpEvent)) {
+            return;
+        }
+        if (this.rect) {
+            this.submit(this.rect);
+        }
+    }
+
+    submit(rect: Rect) {
+        this.addToTree(rect);
+        this.rect = null;
+        this.clearSky();
+    }
+
+    onWheel(wheelEvent: WheelEvent): void {
+        const MIN_STROKE_WIDTH = 1;
+        const MAX_STROKE_WIDTH = 10;
+
+        // 向上滚动
+        let scrollUp = wheelEvent.deltaY < 0;
+        if(scrollUp){
+            this.styleContext.strokeWidth += 1;
+            if(this.styleContext.strokeWidth > MAX_STROKE_WIDTH){
+                this.styleContext.strokeWidth = MAX_STROKE_WIDTH;
+            }
+        }else {
+            this.styleContext.strokeWidth -= 1;
+            if(this.styleContext.strokeWidth < MIN_STROKE_WIDTH){
+                this.styleContext.strokeWidth = MIN_STROKE_WIDTH;
+            }
+        }
+
+        // this.customCursor.style.strokeWidth = this.styleContext.strokeWidth;
+    }
+}
