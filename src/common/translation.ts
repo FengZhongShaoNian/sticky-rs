3361cd269215c2718cae940b4bb8de2e50fc8a71
fengzhongshaonian <2844126834@qq.com>
2024-06-04 16:08:28 +0800

    移除i18next-browser-languagedetector，直接用navigator.language来判断当前的语言

--- src/common/translation.ts
+++ src/common/translation.ts
@@ -1,15 +1,14 @@
 import i18n from 'i18next'
-import LanguageDetector from 'i18next-browser-languagedetector'
 import resources from 'virtual:i18next-loader'
 
-i18n.use(LanguageDetector)
-    .init({
-        resources: {
-            en: { translation: resources.en },
-            zh: { translation: resources['zh-cn'] },
-        },
-        fallbackLng: 'en',
-        debug: true,
-    });
+i18n.init({
+    lng: navigator.language.toLowerCase(),
+    resources: {
+        en: {translation: resources.en},
+        zh: {translation: resources['zh-cn']},
+    },
+    fallbackLng: 'en',
+    debug: true,
+});
 
-export { i18n };
+export {i18n};
