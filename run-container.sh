3b5697ebbb0ce46f1c0eac85d9b790d5441327d3
fengzhongshaonian <2844126834@qq.com>
2024-06-04 20:34:52 +0800

    增加判断容器是否正在运行的逻辑

--- run-container.sh
+++ run-container.sh
@@ -11,8 +11,20 @@ is_container_exists(){
   fi
 }
 
+is_container_running(){
+  local output=$(podman ps --format "table {{.Names}}" | grep sticky_rs)
+    if [[ "$output" == "$container_name" ]];then
+      return 0;
+    else
+      return 1;
+    fi
+}
+
 if ! is_container_exists ;then
   podman run -itd --name "$container_name" -v "$PWD":/workspace   localhost/stick_rs/tauri
 fi
 
+if ! is_container_running ;then
+  podman start "$container_name"
+fi
 podman exec -it "$container_name" bash
