c57e2058d5828ebd13158cb51ff00edb7660e775
fengzhongshaonian <2844126834@qq.com>
2024-08-08 17:20:46 +0800

    解决文本标注工具渲染出来的换行效果和文本框不一致的问题

--- src/pages/main/graphs/text.ts
+++ src/pages/main/graphs/text.ts
@@ -62,18 +62,44 @@ export class Text extends AbstractGraph {
         const content = this._data.content;
         const rows = new Array<string>();
         let rowBuilder = '';
+        let metSpace = false;
+        let word = '';
         for (let i = 0; i < content.length; i++){
             const ch = content.charAt(i);
             if(ch === '\n'){
                 rows.push(rowBuilder);
                 rowBuilder = '';
+            }else if(ch === ' '){
+                // 遇到空格，说明接下来可能是一个单词
+                metSpace = true;
+                rowBuilder += ch;
+                word = '';
             }
             else {
+                if(metSpace){
+                    // 之前遇到空格，如果接下来的字符全部是字母，那么认为它们是单词的一部分
+                    // 否则认为空格后面的这段字串不是单词
+                    if(Text.isLetter(ch)){
+                        word += ch;
+                    }else {
+                        metSpace = false;
+                        word = '';
+                    }
+                }
                 const textMetrics = ctx.measureText(rowBuilder + ch);
                 const textWidth = textMetrics.actualBoundingBoxRight + textMetrics.actualBoundingBoxLeft;
                 if(textWidth > maxWidth){
-                    rows.push(rowBuilder);
-                    rowBuilder = ch;
+                    if(metSpace && word !== ''){
+                        // 是单词，为了避免单词被截断，将最后一个单词移到下一行
+                        rowBuilder += ch;
+                        rows.push(rowBuilder.substring(0, rowBuilder.length - word.length));
+                        rowBuilder = word;
+                    }else {
+                        rows.push(rowBuilder);
+                        rowBuilder = ch;
+                    }
+                    metSpace = false;
+                    word = '';
                 }else {
                     rowBuilder += ch;
                 }
@@ -91,6 +117,10 @@ export class Text extends AbstractGraph {
         }
 
         ctx.restore();
+    }
+
+    static isLetter(char: string) {
+        return /^[a-zA-Z]$/.test(char);
     }
 
     set(data: TextData){
